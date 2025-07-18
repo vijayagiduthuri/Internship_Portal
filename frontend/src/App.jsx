f45afd458c110d334a0de28e402c39873629a0f2
RevanthSrinivas29 <mallarevanthsrinivas@gmail.com>
2025-07-18 22:40:31 +0530

    Created Signup page 01

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import './App.css'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
-    <div>
-      Hello!!
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
