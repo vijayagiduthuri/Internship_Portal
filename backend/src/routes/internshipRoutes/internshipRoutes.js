6015e7e135a5b83a5dc04fce83be0ab20b90755f
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-07-24 15:38:58 +0530

    added the profileModel

--- backend/src/routes/internshipRoutes/internshipRoutes.js
+++ backend/src/routes/internshipRoutes/internshipRoutes.js
@@ -4,19 +4,20 @@ import {
   createInternship,
   updateInternship,
   getAllInternships,
-  getInternshipById 
+  getInternshipById,
+  deleteInternship,
 } from "../../controllers/internshipController/internshipControllers.js";
 
 const router = express.Router();
 
 router.post("/create-internship", createInternship);
 
-router.get("/get-all-internships",getAllInternships );
+router.get("/get-all-internships", getAllInternships);
 
-router.get("/get-internship-id/:id", getInternshipById)
+router.get("/get-internship-id/:id", getInternshipById);
 
 router.put("/update-internship/:id", updateInternship);
 
-router.delete("/delete-internship/:id",deleteInternship );
+router.delete("/delete-internship/:id", deleteInternship);
 
 export default router;
