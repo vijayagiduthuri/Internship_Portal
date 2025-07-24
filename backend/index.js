af572ec7cba43706c2cd94d52483409b8ba043c4
asatvik1958 <asatvik1958@gmail.com>
2025-07-24 16:18:32 +0530

    profile section updated

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import userRoutes from "./src/routes/authUserRoutes/userRoutes.js";
 import authCompanyRoutes from "./src/routes/authCompanyRoutes/authCompanyRoutes.js";
 import internshipRoutes from "./src/routes/internshipRoutes/internshipRoutes.js";
+import profileRoutes from "./src/routes/profileRoutes/profileRoutes.js"
 import { connectDB } from "./src/lib/db.js";
 
 // Load environment variables
@@ -31,6 +32,7 @@ app.use(cookieParser());
 app.use("/api/authUsers", userRoutes);
 app.use("/api/authCompany",authCompanyRoutes )
 app.use("/api/internships", internshipRoutes)
+app.use("api/profile",profileRoutes )
 
 // Set the port from environment variable or default to 9000
 const PORT = process.env.PORT || 9000;
