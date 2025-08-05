64009edc71b83648e685bcc54fd4ef2a8fac9273
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-08-05 21:00:00 +0530

    admin stats implementation

--- backend/src/routes/adminRoutes/adminRoutes.js
+++ backend/src/routes/adminRoutes/adminRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { verifyCompany } from "../../controllers/adminController/adminCompanyVerification.js";
+import { getDashboardStats } from "../../controllers/adminController/adminStats.js";
 
 const router = express.Router();
 
 router.post("/verify-company/:companyId/:verify", verifyCompany);
+
+router.get("/admin-stats", getDashboardStats);
 
 export default router;
